04a6921d20335655ad9972c45ec71723caaae6a8
Dmitry Fedorov <dmitry.fedorov1704@gmail.com>
2025-04-10 18:54:43 +0300

    Step_7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import gameGenerator from '../games/brain-gcd-logic.js';
+import engine from '../src/index.js';
+
+engine(gameGenerator());
---
+++ games/brain-gcd-logic.js
@@ -0,0 +1,33 @@
+// generates game logic for engine
+// see specification of format in comments in engine.js
+
+export default () => {
+    const NUMBER_OF_QUESTIONS = 3;
+    const MAX_VALUE = 10;
+
+    const gameLogic = {};
+    gameLogic.instructions = 'Find the greatest common divisor of given numbers.';
+    const qAndA = [];
+    
+    for (let i = 0; i < NUMBER_OF_QUESTIONS; i += 1) {
+        const num1 = Math.floor(Math.random() * MAX_VALUE);
+        const num2 = Math.floor(Math.random() * MAX_VALUE);
+
+        let a = num1;
+        let b = num2;
+        
+        while (a != 0 & b != 0){
+            if(a > b){  // if a is greater than b, then a is assigned a%b
+                a = a%b;
+            }else{
+                b = b%a;  // do the opposite
+            }
+        }
+
+        const question = `${num1} ${num2}`;
+        const answer = b + a;
+        qAndA.push([question, answer]);        
+    }
+    gameLogic.questions_and_answers = qAndA;
+    return gameLogic;
+}
