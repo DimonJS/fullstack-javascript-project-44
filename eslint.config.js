c8f7674b1cb8469aa8c5ee9879f116c0474bbded
Dmitry Fedorov <dmitry.fedorov1704@gmail.com>
2025-04-15 23:08:12 +0300

    Fix_3

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,37 @@ import pluginJs from '@eslint/js';
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname, 
-    recommendedConfig: pluginJs.configs.recommended });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  ...compat.extends("airbnb"),
+  {
+    languageOptions: {
+      globals: globals.browser,
+
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+
+    },
+  },
+  ...compat.extends('airbnb'),
+  {
+    rules: {
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+    },
+  },
 ];
